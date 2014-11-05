6ea784a081ddd194eb060ad3359da319c5d8434a
Mixer Gutierrez <mixergutierrez@gmail.com>
2014-11-05 11:33:30 -0500

    Add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(set_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
   has_many :projects
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :password, length: { minimum: 6 }, confirmation: true
+
 end
